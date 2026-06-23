1634345dab708f035d6c784ef7804226a782f142
[ progorker ] Zino's Petrel <zinospetrel@sdf.org>
2026-06-23 13:48:24 +0700

    Add files via upload

---
+++ pyWifide/libs/phpsandbox/PhpParser/Node/Scalar/EncapsedStringPart.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace PhpParser\Node\Scalar;
+
+use PhpParser\Node\InterpolatedStringPart;
+
+require_once __DIR__ . '/../InterpolatedStringPart.php';
+
+if (false) {
+    // For classmap-authoritative support.
+    class EncapsedStringPart extends InterpolatedStringPart {
+    }
+}
---
+++ pyWifide/libs/phpsandbox/PhpParser/Node/Scalar/LNumber.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace PhpParser\Node\Scalar;
+
+require_once __DIR__ . '/Int_.php';
+
+if (false) {
+    // For classmap-authoritative support.
+    class LNumber extends Int_ {
+    }
+}
