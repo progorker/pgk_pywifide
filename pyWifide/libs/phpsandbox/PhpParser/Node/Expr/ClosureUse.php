7aa63647954c260af7f87fe5b951259e718395fc
[ progorker ] Zino's Petrel <zinospetrel@sdf.org>
2026-06-23 13:08:14 +0700

    Add files via upload

---
+++ pyWifide/libs/phpsandbox/PhpParser/Node/Expr/ClosureUse.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace PhpParser\Node\Expr;
+
+require_once __DIR__ . '/../ClosureUse.php';
+
+if (false) {
+    // For classmap-authoritative support.
+    class ClosureUse extends \PhpParser\Node\ClosureUse {
+    }
+}
--- pyWifide/libs/phpsandbox/PhpParser/Node/Expr/List_.php
+++ pyWifide/libs/phpsandbox/PhpParser/Node/Expr/List_.php
@@ -5,21 +5,21 @@ namespace PhpParser\Node\Expr;
 use PhpParser\Node\ArrayItem;
 use PhpParser\Node\Expr;
 
-class Array_ extends Expr {
+class List_ extends Expr {
     // For use in "kind" attribute
-    public const KIND_LONG = 1;  // array() syntax
-    public const KIND_SHORT = 2; // [] syntax
+    public const KIND_LIST = 1; // list() syntax
+    public const KIND_ARRAY = 2; // [] syntax
 
-    /** @var ArrayItem[] Items */
+    /** @var (ArrayItem|null)[] List of items to assign to */
     public array $items;
 
     /**
-     * Constructs an array node.
+     * Constructs a list() destructuring node.
      *
-     * @param ArrayItem[] $items Items of the array
+     * @param (ArrayItem|null)[] $items List of items to assign to
      * @param array<string, mixed> $attributes Additional attributes
      */
-    public function __construct(array $items = [], array $attributes = []) {
+    public function __construct(array $items, array $attributes = []) {
         $this->attributes = $attributes;
         $this->items = $items;
     }
@@ -29,6 +29,6 @@ class Array_ extends Expr {
     }
 
     public function getType(): string {
-        return 'Expr_Array';
+        return 'Expr_List';
     }
 }
